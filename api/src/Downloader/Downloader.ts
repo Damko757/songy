65a00e5c576b5c430a07bd63ba08473faf44245a
Damko757 <jambor.damian@gmail.com>
2025-02-17 16:19:44 +0100

    Added ability to choose bitrate and metadatas

--- api/src/Downloader/Downloader.ts
+++ api/src/Downloader/Downloader.ts
@@ -1,6 +1,8 @@
 import ffmpeg from "fluent-ffmpeg";
 import { PassThrough, Readable } from "stream";
 import { Metadator } from "../Metadata/Metadator.js";
+import type { YTDL_DownloadOptions } from "@ybd-project/ytdl-core";
+import type { Metadata } from "../../../shared/Entities/Metadata/Metadata.js";
 
 export class Downloader {
   link: string;
@@ -11,7 +13,12 @@ export class Downloader {
     this.metadator = new Metadator(this.link);
   }
 
-  audioStream(options: object = {}) {
+  audioStream(
+    options: YTDL_DownloadOptions & {
+      metadata?: Partial<Metadata>;
+      bitrate?: number;
+    } = {}
+  ) {
     const self = this;
     const outStream = new PassThrough();
 
@@ -20,36 +27,39 @@ export class Downloader {
       .then((info) => {
         self.metadator
           .getYtDlInstance()
-          .downloadFromInfo(info, { quality: "highestaudio" })
+          .downloadFromInfo(info, { quality: "highestaudio", ...options })
           .then((stream) => {
             const reader = stream.getReader();
-            ffmpeg()
-              .input(
-                new Readable({
-                  async read() {
-                    // Function to read the chunks asynchronously
-                    const pushChunk = async () => {
-                      try {
-                        const { done, value } = await reader.read();
 
-                        if (done) {
-                          this.push(null); // No more data, signal end of stream
-                        } else {
-                          this.push(value); // Push the chunk of data to the Node.js stream
-                          pushChunk(); // Continue reading
+            Metadator.putMetadataToFFMPEG(
+              ffmpeg()
+                .input(
+                  new Readable({
+                    async read() {
+                      // Function to read the chunks asynchronously
+                      const pushChunk = async () => {
+                        try {
+                          const { done, value } = await reader.read();
+
+                          if (done) {
+                            this.push(null); // No more data, signal end of stream
+                          } else {
+                            this.push(value); // Push the chunk of data to the Node.js stream
+                            pushChunk(); // Continue reading
+                          }
+                        } catch (err) {
+                          this.emit("error", err); // Handle any errors
                         }
-                      } catch (err) {
-                        this.emit("error", err); // Handle any errors
-                      }
-                    };
+                      };
 
-                    pushChunk(); // Start reading the chunks
-                  },
-                })
-              )
-              .audioBitrate(320)
-              .format("mp3")
-              .pipe(outStream);
+                      pushChunk(); // Start reading the chunks
+                    },
+                  })
+                )
+                .audioBitrate(options.bitrate ?? 320)
+                .format("mp3"),
+              options.metadata ?? {}
+            ).pipe(outStream);
           })
           .catch((e) => {
             console.error(e);
