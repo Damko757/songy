c77271ff7aea624896afb4d200034a49e620969f
Damko757 <jambor.damian@gmail.com>
2025-02-18 22:40:24 +0100

    Added /metadata endpoint

--- api/src/Controllers/MetadataController.ts
+++ api/src/Controllers/MetadataController.ts
@@ -1,6 +1,8 @@
+import { HttpStatusCode } from "axios";
 import type { MiddlewareFunction } from "../Utils/Entities/MiddlewareFunction";
 import type { RoutingMap } from "../Utils/Router";
 import { RoutableController } from "./Controller";
+import { Metadator } from "../Metadata/Metadator";
 
 /**
  * Controller for managing and finding metadata
@@ -13,5 +15,26 @@ export class MetadataController extends RoutableController {
       },
     };
   }
-  static fetchMetadata(...[req, res, next]: Parameters<MiddlewareFunction>) {}
+  static fetchMetadata(...[req, res, next]: Parameters<MiddlewareFunction>) {
+    const link = (req.query.link as string) ?? "";
+    if (!link)
+      return res.status(HttpStatusCode.BadRequest).send({
+        reason: "(YouTube) link parameter is missing",
+      }) as unknown as void;
+
+    const metadator = new Metadator(link);
+    if (!metadator.isValid())
+      return res.status(HttpStatusCode.BadRequest).send({
+        reason: "(YouTube) link parameter is invalid",
+      }) as unknown as void;
+
+    metadator
+      .metaDatas()
+      .then((metadataResponse) => {
+        res.send(metadataResponse);
+      })
+      .catch((e) => {
+        res.status(HttpStatusCode.InternalServerError).send({ error: e });
+      });
+  }
 }
--- api/src/Utils/RoutableControllers.ts
+++ api/src/Utils/RoutableControllers.ts
@@ -1,4 +1,8 @@
 import { DownloadController } from "../Controllers/DownloadController.ts";
+import { MetadataController } from "../Controllers/MetadataController.ts";
 import type { Routable } from "./Router.ts";
 
-export const routableControllers: Routable[] = [new DownloadController()];
+export const routableControllers: Routable[] = [
+  new DownloadController(),
+  new MetadataController(),
+];
