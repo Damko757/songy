48307889e950829d0c698bb4acbcc5fd82e20628
Damko757 <jambor.damian@gmail.com>
2025-02-18 11:26:34 +0100

    Added sample streaming

--- api/src/Controllers/DownloadController.ts
+++ api/src/Controllers/DownloadController.ts
@@ -2,6 +2,7 @@ import { HttpStatusCode } from "axios";
 import type { MiddlewareFunction } from "../Utils/Entities/MiddlewareFunction";
 import type { RoutingMap } from "../Utils/Router";
 import { RoutableController } from "./Controller";
+import { Downloader } from "../Downloader/Downloader";
 
 /**
  * Controller donwloading file with provided metadata
@@ -9,13 +10,30 @@ import { RoutableController } from "./Controller";
 export class DownloadController extends RoutableController {
   routes(): RoutingMap {
     return {
-      "/": {
-        GET: DownloadController.helloWorld,
+      "/download": {
+        GET: DownloadController.download,
       },
     };
   }
 
-  static helloWorld(...[req, res, next]: Parameters<MiddlewareFunction>) {
-    res.sendStatus(HttpStatusCode.NotImplemented);
+  static download(...[req, res, next]: Parameters<MiddlewareFunction>) {
+    const downloader = new Downloader(
+      "https://youtu.be/hw60VbLttS0?si=xuC_Sm_a-1vNRPLz"
+    );
+    // res.sendStatus(200);
+
+    downloader
+      .audioStream({
+        metadata: {
+          title: "title",
+          artists: ["artist"],
+          album: "album",
+        },
+      })
+      .on("data", (chunk) => res.write(chunk))
+      .on("end", () => {
+        console.log("END");
+        res.end();
+      });
   }
 }
