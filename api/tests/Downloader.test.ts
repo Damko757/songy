d9641e692b0a18944599e9678d4d5c1387a790fb
Damko757 <jambor.damian@gmail.com>
2025-02-18 21:51:07 +0100

    Added video test

--- api/tests/Downloader.test.ts
+++ api/tests/Downloader.test.ts
@@ -1,11 +1,12 @@
-import { expect, describe, it, beforeEach } from "bun:test";
+import { expect, describe, it, beforeEach, beforeAll } from "bun:test";
 import { Downloader } from "../src/Downloader/Downloader";
 import fs from "fs";
 import path, { resolve } from "path";
 import sha256 from "crypto-js/sha256";
 
+const DEBUG_SAVE = false;
 describe("FFMPEG", () => {
-  beforeEach(() => {
+  beforeAll(() => {
     fs.readdir("./out", (err, files) => {
       if (err) throw err;
       for (const file of files) {
@@ -17,7 +18,7 @@ describe("FFMPEG", () => {
     });
   });
 
-  describe.only("MP3", () => {
+  describe("MP3", () => {
     it("Audio only", async () => {
       // expect.assertions(1);
       const downloader = new Downloader(
@@ -31,8 +32,8 @@ describe("FFMPEG", () => {
 
       await new Promise((resolve, reject) => {
         audioStream.on("end", () => {
+          if (DEBUG_SAVE) fs.writeFileSync("out/duck.mp3", Buffer.concat(buf));
           expect(sha256(Buffer.concat(buf).join(""))).toMatchSnapshot();
-          fs.writeFileSync("out/duck.mp3", Buffer.concat(buf));
           resolve(true);
         });
 
@@ -41,13 +42,14 @@ describe("FFMPEG", () => {
         });
       });
     });
-    it.only(
-      "Audio with Metadata",
+    it(
+      "Another audio",
       async () => {
+        /**
+         * Downloader does not automatically provide metadatas
+         */
         const downloader = new Downloader("sduDiIGqvfQ");
-        const metas = await downloader.metadator.metaDatas();
         const audioStream = downloader.audioStream({
-          metadata: metas.spotify?.[0] ?? {},
           bitrate: 320,
         });
 
@@ -58,8 +60,9 @@ describe("FFMPEG", () => {
 
         await new Promise((resolve, reject) => {
           audioStream.on("end", () => {
+            if (DEBUG_SAVE)
+              fs.writeFileSync("out/carpet.mp3", Buffer.concat(buf));
             expect(sha256(Buffer.concat(buf).join(""))).toMatchSnapshot();
-            fs.writeFileSync("out/carpet.mp3", Buffer.concat(buf));
             resolve(true);
           });
 
@@ -76,12 +79,29 @@ describe("FFMPEG", () => {
     it(
       "Video/Audio sync",
       async () => {
-        const downloader = new Downloader(
-          "33fPaNWvyzE" //https://www.youtube.com/watch?v=
-        );
-        await downloader.streamVideo();
+        const downloader = new Downloader("ucZl6vQ_8Uo");
+        const audioStream = downloader.videoStream();
+        const buf: Uint8Array[] = [];
+        audioStream.on("data", (chunk) => {
+          buf.push(chunk);
+        });
+
+        await new Promise((resolve, reject) => {
+          audioStream.on("end", () => {
+            if (DEBUG_SAVE)
+              fs.writeFileSync("out/video.mp4", Buffer.concat(buf));
+            expect(sha256(Buffer.concat(buf).join(""))).toMatchSnapshot();
+            resolve(true);
+          });
+
+          audioStream.on("error", (e) => {
+            reject(e);
+          });
+        });
       },
-      { timeout: 50_000_000 }
+      {
+        timeout: 10_000,
+      }
     );
   });
 });
