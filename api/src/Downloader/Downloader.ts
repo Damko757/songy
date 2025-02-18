8aa87ceb7cb4091b3e5b078cb2a60cce88b001c6
Damko757 <jambor.damian@gmail.com>
2025-02-18 21:51:41 +0100

    Added support to video Downloading

--- api/src/Downloader/Downloader.ts
+++ api/src/Downloader/Downloader.ts
@@ -1,8 +1,16 @@
 import ffmpeg from "fluent-ffmpeg";
-import { PassThrough, Readable } from "stream";
+import { PassThrough, Readable, Writable } from "stream";
 import { Metadator } from "../Metadata/Metadator.js";
-import type { YTDL_DownloadOptions } from "@ybd-project/ytdl-core";
+import type {
+  YTDL_ChooseFormatOptions,
+  YTDL_DownloadOptions,
+  YTDL_VideoFormat,
+} from "@ybd-project/ytdl-core";
 import type { Metadata } from "../../../shared/Entities/Metadata/Metadata.js";
+import { readableStreamToReadable } from "../Utils/ReadableStreamUtils.js";
+import fs from "fs";
+import cp from "child_process";
+import ffmpegPath from "ffmpeg-static";
 
 export class Downloader {
   link: string;
@@ -26,36 +34,113 @@ export class Downloader {
       .then((info) => {
         self.metadator
           .getYtDlInstance()
-          .downloadFromInfo(info, { quality: "highestaudio", ...options })
+          .downloadFromInfo(info, {
+            quality: options.quality ?? "highestaudio",
+            filter: "audio",
+            ...options,
+          })
           .then((stream) => {
-            const reader = stream.getReader();
+            const readable = readableStreamToReadable(stream);
             ffmpeg()
-              .input(
-                new Readable({
-                  async read() {
-                    // Function to read the chunks asynchronously
-                    const pushChunk = async () => {
-                      try {
-                        const { done, value } = await reader.read();
-
-                        if (done) {
-                          this.push(null); // No more data, signal end of stream
-                        } else {
-                          this.push(value); // Push the chunk of data to the Node.js stream
-                          pushChunk(); // Continue reading
-                        }
-                      } catch (err) {
-                        this.emit("error", err); // Handle any errors
-                      }
-                    };
-
-                    pushChunk(); // Start reading the chunks
-                  },
-                })
-              )
+              .input(readable)
               .audioBitrate(options.bitrate ?? 320)
               .format("mp3")
               .pipe(outStream);
+          })
+          .catch((e) => {
+            console.error(e);
+            outStream.emit("error", e);
+          });
+      })
+      .catch((e) => {
+        outStream.emit("error", e);
+      });
+
+    return outStream;
+  }
+
+  videoStream(
+    options: YTDL_DownloadOptions & {
+      bitrate?: number;
+      videoQuality?: YTDL_ChooseFormatOptions["quality"];
+      audioQuality?: YTDL_ChooseFormatOptions["quality"];
+    } = {}
+  ) {
+    const self = this;
+    const outStream = new PassThrough();
+
+    this.metadator
+      .rawMetaData()
+      .then((info) => {
+        const audioStreamPromise = self.metadator
+          .getYtDlInstance()
+          .downloadFromInfo(info, {
+            quality: options.audioQuality ?? "highestaudio",
+            filter: "audio",
+            ...options,
+          });
+        const videoStreamPromise = self.metadator
+          .getYtDlInstance()
+          .downloadFromInfo(info, {
+            quality: "highestvideo",
+            filter: "video",
+            ...options,
+          });
+        Promise.all([audioStreamPromise, videoStreamPromise])
+          .then(([audioStream, videoStream]) => {
+            const audioReadable = readableStreamToReadable(audioStream);
+            const videoReadable = readableStreamToReadable(videoStream);
+
+            // audioReadable.pipe(fs.createWriteStream("out/sound.mp4"));
+            // videoReadable.pipe(fs.createWriteStream("out/vid.mp4"));
+            // videoReadable.on("end", () => outStream.end());
+
+            const ffmpegProcess = cp.spawn(
+              ffmpegPath!,
+              [
+                // supress non-crucial messages
+                "-loglevel",
+                "8",
+                "-hide_banner",
+                "-fflags",
+                "+igndts",
+                // input audio and video by pipe
+                "-i",
+                "pipe:3",
+                "-i",
+                "pipe:4",
+                // map audio and video correspondingly
+                "-map",
+                "0:a",
+                "-map",
+                "1:v",
+                // no need to change the codec
+                "-c",
+                "copy",
+                // output mp4 and pipe
+                "-f",
+                "matroska",
+                "pipe:5",
+              ],
+              {
+                // no popup window for Windows users
+                windowsHide: true,
+                stdio: [
+                  // silence stdin/out, forward stderr,
+                  "inherit",
+                  "inherit",
+                  "inherit",
+                  // and pipe audio, video, output
+                  "pipe",
+                  "pipe",
+                  "pipe",
+                ],
+              }
+            );
+            audioReadable.pipe(ffmpegProcess.stdio[3] as Writable);
+            videoReadable.pipe(ffmpegProcess.stdio[4] as Writable);
+
+            (ffmpegProcess.stdio as Readable[])[5].pipe(outStream);
           })
           .catch((e) => {
             console.error(e);
