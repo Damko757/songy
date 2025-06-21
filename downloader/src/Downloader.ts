1b121e709cf00142d6bf3fbb18d470288ace3692
Damko757 <jambor.damian@gmail.com>
2025-06-21 14:13:33 +0200

    Implemented videoOnly downloading

--- downloader/src/Downloader.ts
+++ downloader/src/Downloader.ts
@@ -60,11 +60,53 @@ export class Downloader {
   }
 
   /**
+   * Connects `source` and `target` streams with events
+   * @param sourceStream Stream to listen to events from
+   * @param targetStream Stream to emit the events on
+   */
+  protected pipeStreams(sourceStream: Readable, targetStream: Writable) {
+    targetStream.on("error", (...args) => sourceStream.emit("close", ...args));
+    targetStream.on("close", (...args) => sourceStream.emit("close", ...args));
+    targetStream.on("finish", (...args) =>
+      sourceStream.emit("finish", ...args)
+    );
+    targetStream.on("end", (...args) => sourceStream.emit("end", ...args));
+    targetStream.on("progress", (...args) =>
+      sourceStream.emit("progress", ...args)
+    );
+    targetStream.on("info", (...args) => sourceStream.emit("info", ...args));
+    sourceStream.pipe(targetStream);
+  }
+
+  /**
+   * Download video Only
+   * @param options
+   */
+  videoStream(options: downloadOptions = {}) {
+    const outStream = new PassThrough();
+
+    ytdl
+      .getInfo(this.link)
+      .then((info) => {
+        const videoStream = ytdl.downloadFromInfo(info, {
+          filter: "video",
+          ...options,
+        });
+        this.pipeStreams(videoStream, outStream);
+      })
+      .catch((e) => {
+        outStream.emit("error", e);
+      });
+
+    return outStream;
+  }
+
+  /**
    * Streams video + audio from youtube
    * @param options
    * @returns PassThrough stream with video stream
    */
-  videoStream(
+  combinedStream(
     options: downloadOptions & {
       bitrate?: number;
       videoQuality?: ytdl.chooseFormatOptions["quality"];
@@ -77,22 +119,62 @@ export class Downloader {
     ytdl
       .getInfo(this.link)
       .then((info) => {
-        // ytdl.downloadFromInfo(info, {
-        //   quality: options.audioQuality ?? "highestaudio",
-        //   filter: "audio",
-        //   ...options,
-        // });
-        const stream = ytdl.downloadFromInfo(info, {
-          quality: "highestvideo",
+        const audioStream = ytdl.downloadFromInfo(info, {
+          quality: options.audioQuality ?? "highestaudio",
+          filter: "audio",
+          ...options,
+        });
+        const videoStream = ytdl.downloadFromInfo(info, {
+          quality: options.audioQuality ?? "highestvideo",
           // filter: "video",
           ...options,
         });
-        stream.on("close", (...args) => outStream.emit("close", ...args));
-        stream.on("finish", (...args) => outStream.emit("finish", ...args));
-        stream.on("end", (...args) => outStream.emit("end", ...args));
-        stream.on("progress", (...args) => outStream.emit("progress", ...args));
-        stream.on("info", (...args) => outStream.emit("info", ...args));
-        stream.pipe(outStream);
+        const ffmpegProcess = cp.spawn(
+          ffmpegPath!,
+          [
+            // supress non-crucial messages
+            "-loglevel",
+            "8",
+            "-hide_banner",
+            "-fflags",
+            "+igndts",
+            // input audio and video by pipe
+            "-i",
+            "pipe:3",
+            "-i",
+            "pipe:4",
+            // map audio and video correspondingly
+            "-map",
+            "0:a",
+            "-map",
+            "1:v",
+            // no need to change the codec
+            "-c",
+            "copy",
+            // output mp4 and pipe
+            "-f",
+            "matroska",
+            "pipe:5",
+          ],
+          {
+            // no popup window for Windows users
+            windowsHide: true,
+            stdio: [
+              // silence stdin/out, forward stderr,
+              "inherit",
+              "inherit",
+              "inherit",
+              // and pipe audio, video, output
+              "pipe",
+              "pipe",
+              "pipe",
+            ],
+          }
+        );
+
+        audioStream.pipe(ffmpegProcess.stdio[3] as Writable);
+        videoStream.pipe(ffmpegProcess.stdio[4] as Writable);
+        (ffmpegProcess.stdio as Readable[])[5].pipe(outStream);
 
         // Promise.all([audioStreamPromise, videoStreamPromise])
         //   .then(([audioStream, videoStream]) => {
@@ -103,47 +185,7 @@ export class Downloader {
         //     // videoReadable.pipe(fs.createWriteStream("out/vid.mp4"));
         //     // videoReadable.on("end", () => outStream.end());
 
-        //     const ffmpegProcess = cp.spawn(
-        //       ffmpegPath!,
-        //       [
-        //         // supress non-crucial messages
-        //         "-loglevel",
-        //         "8",
-        //         "-hide_banner",
-        //         "-fflags",
-        //         "+igndts",
-        //         // input audio and video by pipe
-        //         "-i",
-        //         "pipe:3",
-        //         "-i",
-        //         "pipe:4",
-        //         // map audio and video correspondingly
-        //         "-map",
-        //         "0:a",
-        //         "-map",
-        //         "1:v",
-        //         // no need to change the codec
-        //         "-c",
-        //         "copy",
-        //         // output mp4 and pipe
-        //         "-f",
-        //         "matroska",
-        //         "pipe:5",
-        //       ],
-        //       {
-        //         // no popup window for Windows users
-        //         windowsHide: true,
-        //         stdio: [
-        //           // silence stdin/out, forward stderr,
-        //           "inherit",
-        //           "inherit",
-        //           "inherit",
-        //           // and pipe audio, video, output
-        //           "pipe",
-        //           "pipe",
-        //           "pipe",
-        //         ],
-        //       }
+        //
         //     );
         //     audioReadable.pipe(ffmpegProcess.stdio[3] as Writable);
         //     videoReadable.pipe(ffmpegProcess.stdio[4] as Writable);
--- downloader/tests/Downloader.test.ts
+++ downloader/tests/Downloader.test.ts
@@ -3,100 +3,104 @@ import fs from "fs";
 import path, { resolve } from "path";
 import { beforeAll, describe, expect, it } from "@jest/globals";
 import { sha256 } from "sha.js";
+import ytdl from "@distube/ytdl-core";
 
 const DEBUG_SAVE = true;
-describe("FFMPEG", () => {
-  beforeAll(() => {
-    fs.readdir("./out", (err, files) => {
-      if (err) throw err;
-      // for (const file of files) {
-      //   if (file.endsWith(".jpg")) continue;
-      //   fs.unlink(path.join("./out", file), (err) => {
-      //     if (err) throw err;
-      //   });
-      // }
+beforeAll(() => {
+  const files = fs.readdirSync("./out");
+  // for (const file of files) {
+  //   if (file.endsWith(".jpg")) continue;
+  //   fs.unlinkSync(path.join("./out", file));
+  // }
+});
+
+describe("Audio only", () => {
+  it("Audio only", async () => {
+    // expect.assertions(1);
+    const downloader = new Downloader(
+      "33fPaNWvyzE" //https://www.youtube.com/watch?v=
+    );
+    const audioStream = downloader.audioStream();
+    const buf: Uint8Array[] = [];
+    audioStream.on("data", (chunk) => {
+      buf.push(chunk);
+    });
+
+    await new Promise((resolve, reject) => {
+      audioStream.on("end", () => {
+        if (DEBUG_SAVE) fs.writeFileSync("out/duck.mp3", Buffer.concat(buf));
+        expect(
+          new sha256().update(Buffer.concat(buf).join(""))
+        ).toMatchSnapshot();
+        resolve(true);
+      });
+
+      audioStream.on("error", (e) => {
+        reject(e);
+      });
     });
   });
+  it("Another audio", async () => {
+    /**
+     * Downloader does not automatically provide metadatas
+     */
+    const downloader = new Downloader("sduDiIGqvfQ");
+    const audioStream = downloader.audioStream({
+      bitrate: 320,
+    });
 
-  describe("MP3", () => {
-    it("Audio only", async () => {
-      // expect.assertions(1);
-      const downloader = new Downloader(
-        "33fPaNWvyzE" //https://www.youtube.com/watch?v=
-      );
-      const audioStream = downloader.audioStream();
-      const buf: Uint8Array[] = [];
-      audioStream.on("data", (chunk) => {
-        buf.push(chunk);
+    const buf: Uint8Array[] = [];
+    audioStream.on("data", (chunk) => {
+      buf.push(chunk);
+    });
+
+    await new Promise((resolve, reject) => {
+      audioStream.on("end", () => {
+        if (DEBUG_SAVE) fs.writeFileSync("out/carpet.mp3", Buffer.concat(buf));
+        expect(
+          new sha256().update(Buffer.concat(buf).join(""))
+        ).toMatchSnapshot();
+        resolve(true);
       });
 
-      await new Promise((resolve, reject) => {
-        audioStream.on("end", () => {
-          if (DEBUG_SAVE) fs.writeFileSync("out/duck.mp3", Buffer.concat(buf));
-          expect(
-            new sha256().update(Buffer.concat(buf).join(""))
-          ).toMatchSnapshot();
-          resolve(true);
-        });
-
-        audioStream.on("error", (e) => {
-          reject(e);
-        });
+      audioStream.on("error", (e) => {
+        reject(e);
       });
     });
-    it("Another audio", async () => {
-      /**
-       * Downloader does not automatically provide metadatas
-       */
-      const downloader = new Downloader("sduDiIGqvfQ");
-      const audioStream = downloader.audioStream({
-        bitrate: 320,
-      });
+  }, 10_000);
+});
 
-      const buf: Uint8Array[] = [];
-      audioStream.on("data", (chunk) => {
-        buf.push(chunk);
-      });
-
-      await new Promise((resolve, reject) => {
-        audioStream.on("end", () => {
-          if (DEBUG_SAVE)
-            fs.writeFileSync("out/carpet.mp3", Buffer.concat(buf));
-          expect(
-            new sha256().update(Buffer.concat(buf).join(""))
-          ).toMatchSnapshot();
-          resolve(true);
-        });
-
-        audioStream.on("error", (e) => {
-          reject(e);
-        });
-      });
-    }, 10_000);
-  });
-
-  describe.only("Video only", () => {
-    it("Simple video", async () => {
-      const downloader = new Downloader(
-        "https://www.youtube.com/watch?v=FTQbiNvZqaY"
+describe("Video only", () => {
+  const url = "https://www.youtube.com/watch?v=aqz-KE-bpKQ"; // Blender official film
+  for (const res of [144, 240, 360, 480, 720, 1080, 1440, 2160]) {
+    // Testing all video files
+    it(`${res}p`, async () => {
+      const downloader = new Downloader(url);
+      const format = (await ytdl.getInfo(url)).formats.find(
+        (f) => f.height === res && f.hasVideo && !f.hasAudio
       );
-      const videoStream = downloader.videoStream();
-      console.log(videoStream);
+      expect(format).toBeDefined();
+
+      const videoStream = downloader.videoStream({
+        format: format,
+      });
+
       const buf: Uint8Array[] = [];
-      const file = fs.createWriteStream("out/simple.mp4");
+      const file = DEBUG_SAVE
+        ? fs.createWriteStream(`out/${res}p.mp4`)
+        : undefined;
       videoStream.on("data", (chunk) => {
-        buf.push(chunk);
-        if (DEBUG_SAVE) file.write(chunk);
+        buf.push(chunk); // Saving chunk to buffer
+        file?.write(chunk); // Writing chunk (if needed)
       });
 
       return new Promise<void>((resolve, reject) => {
         videoStream.on("end", () => {
-          console.log("END!");
-          file.end();
+          file?.end();
 
-          // expect(
-          //   new sha256().update(Buffer.concat(buf).join(""))
-          // ).toMatchSnapshot();
+          expect(
+            new sha256().update(Buffer.concat(buf).join(""))
+          ).toMatchSnapshot();
           resolve();
         });
 
@@ -104,33 +108,33 @@ describe("FFMPEG", () => {
           reject(e);
         });
       });
-    }, 1_000_000);
-  });
+    }, 10_000); // 10s
+  }
+});
 
-  describe("MP4", () => {
-    it("Video/Audio sync", async () => {
-      const downloader = new Downloader(
-        "https://youtu.be/LXb3EKWsInQ?si=kE2iqF0famt_njjk"
-      );
-      const audioStream = downloader.videoStream();
-      const buf: Uint8Array[] = [];
-      audioStream.on("data", (chunk) => {
-        buf.push(chunk);
+describe("Combined", () => {
+  it("Video/Audio sync", async () => {
+    const downloader = new Downloader(
+      "https://youtu.be/LXb3EKWsInQ?si=kE2iqF0famt_njjk"
+    );
+    const audioStream = downloader.videoStream();
+    const buf: Uint8Array[] = [];
+    audioStream.on("data", (chunk) => {
+      buf.push(chunk);
+    });
+
+    await new Promise((resolve, reject) => {
+      audioStream.on("end", () => {
+        if (DEBUG_SAVE) fs.writeFileSync("out/video.mp4", Buffer.concat(buf));
+        expect(
+          new sha256().update(Buffer.concat(buf).join(""))
+        ).toMatchSnapshot();
+        resolve(true);
       });
 
-      await new Promise((resolve, reject) => {
-        audioStream.on("end", () => {
-          if (DEBUG_SAVE) fs.writeFileSync("out/video.mp4", Buffer.concat(buf));
-          expect(
-            new sha256().update(Buffer.concat(buf).join(""))
-          ).toMatchSnapshot();
-          resolve(true);
-        });
-
-        audioStream.on("error", (e) => {
-          reject(e);
-        });
+      audioStream.on("error", (e) => {
+        reject(e);
       });
-    }, 10_0000);
-  });
+    });
+  }, 10_0000);
 });
