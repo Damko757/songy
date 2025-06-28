25beeee69effc57ea2bf8805bce0f3c4817c7e76
Damko757 <jambor.damian@gmail.com>
2025-06-28 22:29:29 +0200

    Added metadata commenets

--- shared/Entities/Metadata/Metadata.ts
+++ shared/Entities/Metadata/Metadata.ts
@@ -1,9 +1,8 @@
-export interface Metadata {
-  title: string;
-  artists: string[];
-  album: string | null;
-  thumbnails: { url: string; width: number; height: number }[];
-  releaseDate: string | null; // ISO date
-  duration: number; // in ms
-  lyrics?: string;
+export interface AudioMetadata {
+  title: string; ///< Song title (Africe)
+  artists: string[]; ///< Song Artists ([Toto])
+  album: string | null; //< Album name
+  thumbnails: { url: string; width: number; height: number }[]; ///< Thumbnails with path to obtain it an dimensions
+  releaseDate: string | null; ///< ISO date
+  lyrics?: string; ///< Song's lyrics
 }
