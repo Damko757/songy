e96167d3c040a3982be4148096ae64b927e6f8ad
Damko757 <jambor.damian@gmail.com>
2025-06-30 11:30:08 +0200

    Created MediaFile Schema

--- api/src/Database/Schemas/MediaFile.ts
+++ api/src/Database/Schemas/MediaFile.ts
@@ -1,6 +1,5 @@
 import mongoose, { type ObjectId } from "mongoose";
 import type { DownloadJob } from "../../../../downloader/src/Commands/Command.js";
-import type { AudioMetadata } from "../../../../shared/Entities/Metadata/Metadata.js";
 
 /**
  * Sctructure representing State of downloaded/downloading file
@@ -11,19 +10,42 @@ export interface MediaFileExtension {
 }
 
 export enum MediaFileState {
-  WAITING, ///< Waiting in queue
-  DOWNLOADING, ///< Downloading of file started, WS is notified about progress
-  PROCESSING, ///< Download 100%, but not finished. FFMPEG processing and/or binding Metadata
-  READY, ///< File ready to be downloading
+  WAITING = "STATE_WAITING", ///< Waiting in queue
+  DOWNLOADING = "STATE_DOWNLOADING", ///< Downloading of file started, WS is notified about progress
+  PROCESSING = "STATE_PROCESSING", ///< Download 100%, but not finished. FFMPEG processing and/or binding Metadata
+  READY = "STATE_READY", ///< File ready to be downloading
 }
 
 export interface MediaFile extends DownloadJob {
-  _id: ObjectId;
-  name?: string; ///< Filename (when downloaded)
+  name: string; ///< Filename (when downloaded)
   state: MediaFileState; ///< State about file
 }
 
-const MediaFileSchema = new mongoose.Schema({});
+/**
+ * @see AudioMetadata
+ */
+const AudioMetadataSchema = new mongoose.Schema({
+  title: { type: String },
+  artists: { type: [String] },
+  album: { type: String, default: null },
+  thumbnails: { type: [{ url: String, width: Number, height: Number }] },
+  releaseDate: { type: String, default: null },
+  lyrics: { type: String, default: undefined, required: false },
+});
+
+const MediaFileSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  state: { type: String, enum: Object.values(MediaFileState), required: true },
+  extension: { type: String },
+  link: { type: String },
+  type: { type: String },
+  options: { type: mongoose.Schema.Types.Mixed, default: {} },
+  audioMetadata: {
+    type: AudioMetadataSchema,
+    required: false,
+    default: undefined,
+  },
+});
 
 export const MediaFileModel = mongoose.model<MediaFile>(
   "MediaFile",
