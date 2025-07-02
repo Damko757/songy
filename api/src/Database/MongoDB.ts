8fa82aefd7b5b2d15c0cbc755a1b308d1cf52ecf
Damko757 <jambor.damian@gmail.com>
2025-07-02 20:11:19 +0200

    Mongo now keeps track of running connections

--- api/src/Database/MongoDB.ts
+++ api/src/Database/MongoDB.ts
@@ -2,21 +2,23 @@ import mongoose, { Mongoose } from "mongoose";
 import chalk from "chalk";
 import { ENV } from "../env";
 
+let instances = 0; // Currently runnning instances
+
 /**
  * Connects to docker mongoDB instance
  * @param options Connection options
  * @param quiet If should print "connection successfull" message
- * @returns Promise resolved with current istance
+ * @returns Promise resolved with current number of instances
  */
 export const connectMongoose = async (
   options: { quiet?: boolean } = {
     quiet: true,
   }
 ) => {
-  return new Promise<typeof mongoose>((resolve, rejected) => {
+  return new Promise<number>((resolve, rejected) => {
     // Mongo Running, no need to connect
 
-    const connectString = `mongodb://${ENV.MONGODB_DATABASE_URI}:${ENV.MONGODB_DATABASE_PORT}`;
+    const connectString = `mongodb://${ENV.MONGODB_DATABASE_USER}:${ENV.MONGODB_DATABASE_PASSWORD}@${ENV.MONGODB_DATABASE_URI}:${ENV.MONGODB_DATABASE_PORT}`;
 
     mongoose
       .set("strictQuery", false)
@@ -33,11 +35,26 @@ export const connectMongoose = async (
           );
         }
 
-        resolve(mongooseInstance);
+        resolve(++instances);
       })
       .catch((err) => {
         console.error("Unable to connect to MongoDB!");
         rejected(err);
       });
   });
+};
+
+/**
+ * Disconnects from Mongoose. Keeps tracks of instances, so it is destroyed only if all instances ended
+ * @return Number of left instances
+ */
+export const destroyMongoose = async () => {
+  if (instances == 0)
+    throw new Error("No running instance, synchronization problem maybe?");
+  if (--instances == 0) {
+    // No active client, disconnecting for real
+    await mongoose.disconnect();
+  }
+
+  return instances;
 };
