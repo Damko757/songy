29d865c334e63b358ae1a96ee32a9a4244abb63a
Damko757 <jambor.damian@gmail.com>
2025-07-05 22:18:54 +0200

    Added SIGINT behaviour Added retrying in Downloader connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,6 +5,7 @@ import { ENV } from "./env.ts";
 import { MediaFileModel } from "./Database/Schemas/MediaFile.ts";
 import { APISocketServer } from "./WebSocket/APISocketServer.ts";
 import { DownloaderCommandType } from "../../downloader/src/Commands/Command.ts";
+import { Utils } from "./Utils/Utils.ts";
 
 console.log(chalk.blueBright(`Welcome! ${new Date()}`));
 
@@ -15,9 +16,35 @@ const socketServer = new APISocketServer(
 Promise.all([
   connectMongoose({ quiet: false }),
   initApp(),
-  socketServer.bindWSS(),
+  // WebSocket connection retry
+  new Promise(async (resolve, reject) => {
+    let lastError: ErrorEvent;
+    while (true) {
+      try {
+        return resolve(await socketServer.bindWSS());
+      } catch (e) {
+        lastError = e as ErrorEvent;
+        // If unable to connect, try again after 5s
+        // Else throw error
+        if (
+          (e as ErrorEvent).message.match(
+            /^WebSocket connection to '\S*' failed: Failed to connect\.?$/
+          )
+        ) {
+          console.log(chalk.red("Unable to connect"));
+          await Utils.sleep(5_000); // Sleep for 5s
+          console.log(chalk.yellow("Retrying connection..."));
+          continue;
+        }
+
+        break;
+      }
+    }
+
+    reject(lastError);
+  }),
 ])
-  .then(([mongooseInstances, app, wssBindSuccess]) => {
+  .then(([mongooseInstances, app, wasWSSAlreadyRunning]) => {
     // Everything is running
     app
       .listen(ENV.API_PORT)
@@ -32,3 +59,10 @@ Promise.all([
     console.error(err);
     destroyMongoose();
   });
+
+process.on("SIGINT", () => {
+  console.log(chalk.blue("\rClosing connection"));
+  socketServer.destroy();
+  destroyMongoose();
+  process.exit();
+});
--- downloader/src/index.ts
+++ downloader/src/index.ts
@@ -1,55 +1,32 @@
-import { WebSocket } from "ws";
+import chalk from "chalk";
 import { CommandProcessor } from "./Commands/CommandProcessor.js";
-import { ObjectId } from "mongoose";
-import {
-  DownloaderCommand,
-  DownloaderCommandResponse,
-  DownloaderCommandResponseType,
-  DownloaderCommandType,
-} from "./Commands/Command.js";
-import dotenv from "dotenv";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
 import { connectMongoose } from "./Database/MongoDB.js";
-import { ENV } from "./env.js";
+
+let SING_INT_COUNT = 0; // If Program is ending
+
+const mongoose = await connectMongoose();
 
 const commandProcessor = new CommandProcessor();
+commandProcessor.bindWSS();
 
-// const client = new WebSocket("ws://127.0.0.1:" + ENV.DOWNLOADER_WS_PORT);
-// client.on("error", console.error);
-
-// client.on("message", async (message: string) => {
-//   const data = JSON.parse(message) as DownloaderCommandResponse;
-//   if (data.type == DownloaderCommandResponseType.ERROR)
-//     return client.emit("error", data.error);
-
-//   if (data.type == DownloaderCommandResponseType.START) {
-//     for (let i = 0; i < 5; i++) {
-//       const command: DownloaderCommand = {
-//         action: DownloaderCommandType.DOWNLOAD,
-//         extension: "mp4",
-//         _id: `${"ABCDE"[i]}` as unknown as ObjectId,
-//         link: "https://www.youtube.com/watch?v=aqz-KE-bpKQ&t=30s",
-//         options: { video: { quality: "highest" } },
-//         type: "video",
-//       };
-//       client.send(JSON.stringify(command));
-
-//       await new Promise((resolve) => setTimeout(resolve, 1000));
-//       break;
-//     }
-//   }
-//   // else if(data.type == DownloaderCommandResponseType.){}
-// });
-
-// client.on("open", () => {
-//   console.log("client connected!");
-
-//   const startCommand: DownloaderCommand = {
-//     action: DownloaderCommandType.START,
-//     numberOfWorkers: 5,
-//   };
-//   // client.send(JSON.stringify(startCommand));
-// });
-
-const mongooseInstance = connectMongoose({ quiet: false });
+process.on("SIGINT", () => {
+  SING_INT_COUNT++;
+  if (SING_INT_COUNT == 1) {
+    console.log(chalk.blue("\rClosing connection"));
+    commandProcessor.destroy().then(async () => {
+      await mongoose.disconnect();
+      process.exit();
+    });
+  } else if (SING_INT_COUNT == 2) {
+    console.log(
+      chalk.rgb(
+        255,
+        128,
+        0
+      )("\rAlready closing connection. Press again to force...")
+    );
+  } else {
+    console.log(chalk.redBright("\rForceful end"));
+    process.exit();
+  }
+});
